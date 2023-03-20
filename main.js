b4a142603142a45f434b8c473fb680cc19ffeda1
Evgeniy1109 <q126p@yandex.ru>
2023-03-20 15:13:28 +0300

    Изменения на 20.03 15:15

--- main.js
+++ main.js
@@ -88,3 +88,32 @@ let swiper = new Swiper(".mySwiper", {
     pauseSlideshow();
     previousSlide();
   };
+
+  // Кавказ
+const tab = document.querySelectorAll('.tab_header');
+const caucasus = document.querySelectorAll('.caucasus')
+
+
+for (let i=0; i<tab.length; i++){
+    tab[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        let activeTab = e.target.getAttribute('data-tab');
+        for (let j=0; j<tab.length; j++){
+            let content = caucasus[j].getAttribute('data-tab-content');
+
+            if (activeTab === content){
+                tab[j].classList.add('active');
+                caucasus[j].classList.add('active');
+            }else{
+                tab[j].classList.remove('active');
+                caucasus[j].classList.remove('active');
+            }
+        }
+    })
+}
+// ----------------------------
+
+
+// Крым
+
+// ---------------------
